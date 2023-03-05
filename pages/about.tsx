e97f2fa6cb80d52c92e3a8a0a9a265303eefbf80
Daiki <daiki.miura@prum.jp>
2023-03-05 16:17:59 +0900

    fix: login etc

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,10 @@
 import type { NextPage } from 'next'
+import { Alert, Snackbar } from "@mui/material"
+import { useAuthContext } from "../context/AuthContext"
 
 const About: NextPage = () => {
+  const { currentUser } = useAuthContext()
+  const isLoggedIn = !!currentUser
   return (
     <div>
       <h1 className="text-blue-700 underline decoration-gray-500">
@@ -9,6 +13,14 @@ const About: NextPage = () => {
       <p>
         add CircleCI!
       </p>
+      <Snackbar
+        open={!isLoggedIn}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        autoHideDuration={3000}
+        key={"top" + "center"}
+      >
+        <Alert severity="warning">ログインしてください</Alert>
+      </Snackbar>
     </div>
   )
 }
