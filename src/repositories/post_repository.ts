38df8af8473037d36cf3d1fe829658f957377210
Daiki <daiki.miura@prum.jp>
2023-03-11 19:12:18 +0900

    add: api通信（WIP）

---
+++ src/repositories/post_repository.ts
@@ -0,0 +1,38 @@
+import { ApiClient } from "../lib/api-client"
+import { PostType } from "../models/post_model"
+
+export type PostRepository = {
+  getPosts: () => Promise<PostType[]>
+  getPost: (params: Pick<PostType, "id">) => Promise<PostType>
+  createPost: (
+    params: Omit<PostType, "id">
+  ) => Promise<void>
+  deletePost: (params: Pick<PostType, "id">) => Promise<void>
+}
+
+const getPosts = async (): Promise<PostType[]> => {
+  const response = await ApiClient.get(`/posts`)
+  return response.data
+}
+
+const getPost = async (params: Pick<PostType, "id">): Promise<PostType> => {
+  const response = await ApiClient.get(`/posts/${params.id}`)
+  return response.data
+}
+
+const createPost = async (
+  params: Omit<PostType, "id">
+) => {
+  await ApiClient.post(`/posts`, params)
+}
+
+const deletePost = async (params: Pick<PostType, "id">) => {
+  await ApiClient.post(`/posts/${params.id}`, params)
+}
+
+export const postRepository: PostRepository = {
+  getPosts,
+  getPost,
+  createPost,
+  deletePost,
+}
