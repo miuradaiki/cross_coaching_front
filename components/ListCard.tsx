4dab1b772ed5bca499feae334acb0954fb69b3b2
Daiki <daiki.miura@prum.jp>
2023-06-18 22:51:31 +0900

    add: styles

---
+++ components/ListCard.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import { QuestionType } from "../src/models/question_model"
+
+type Props = {
+  question: QuestionType
+}
+
+export const ListCard: React.FC<Props> = (props) => {
+  const { question } = props
+
+  return (
+    <Box>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography sx={{ fontSize: 18 }} gutterBottom>
+            {question.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {question.description}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  )
+}
+
+export default ListCard
--- src/features/question/components/Questions.tsx
+++ src/features/question/components/Questions.tsx
@@ -1,6 +1,11 @@
 import React from "react"
 import { QuestionType } from "../../../models/question_model"
 import Link from "next/link"
+import { ListCard } from "../../../../components/ListCard"
+import styles from '@/styles/Question.module.css'
+import { spacing } from '@mui/system'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 
 type Props = {
   questions: QuestionType[]
@@ -11,16 +16,23 @@ export const Questions: React.FC<Props> = (props) => {
   return (
     <>
       <div>
-        <ul>
-          {props.questions && props.questions.map((i) => (
-            <li key={i.id}>
-              <Link href={`/questions/${i.id}`}>
-                {i.title}
-              </Link>
-              <span></span>
-            </li>
-          ))}
-        </ul>
+        <Container sx={{ maxWidth: 800 }} className={styles.container}>
+          <div className={styles.title}>Self-Coaching Questions</div>
+          <Box sx={{ mt: 4 }}>
+            <ul>
+              {props.questions &&
+                props.questions.map((question) => (
+                  <Box sx={{ my: 1 }} key={question.id}>
+                    <li>
+                      <Link href={`/questions/${question.id}`}>
+                        <ListCard question={question} />
+                      </Link>
+                    </li>
+                  </Box>
+                ))}
+            </ul>
+          </Box>
+        </Container>
       </div>
     </>
   )
