e85d8de7439e6e4a8d6da36f5eb4e3f9ec5dd4c5
Daiki <daiki.miura@prum.jp>
2023-07-23 13:03:14 +0900

    fix: shares detail page style

--- src/features/share/components/Share.tsx
+++ src/features/share/components/Share.tsx
@@ -10,6 +10,8 @@ import axios from "axios"
 import { useRouter } from "next/router"
 import { VoteButton } from "src/features/vote/components/vote_button"
 import { useState, useEffect } from "react"
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 
 export type Props = {
   question?: QuestionType
@@ -96,74 +98,75 @@ export const Share: React.FC<Props> = (props: Props) => {
         <title>シェア詳細</title>
         <meta name="description" content={props.answer.description} />
       </Head>
-      <div className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              {props.question?.title}
-            </h1>
+
+      <Container sx={{ maxWidth: 800 }}>
+        <div className="text-gray-600 body-font mt-8">
+          <div className="container mx-auto flex md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                {props.question?.title}
+              </h1>
+            </div>
+          </div>
+          <div className="container mx-auto">
+            {props.answer.description}
+          </div>
+          <div className="mt-8">- Feedbacks -</div>
+          <div>
+            <ul>
+              {props.feedbacks.map((feedback) => (
+                <li key={feedback.id}>
+                  {feedback.description}
+                  <span></span>
+                  <VoteButton
+                    feedbackId={feedback.id}
+                    userId={currentUser?.uid}
+                    totalUpVotes={feedback.total_up_votes}
+                    totalDownVotes={feedback.total_down_votes}
+                    totalVotes={feedback.total_votes}
+                    isVoted={votedFeedbackIds.includes(feedback.id)}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* フォーム */}
+          <div className="container mx-auto mt-8">
+            <div className="lg:w-1/2 md:w-2/3 mx-auto">
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <div className="p-2 w-full">
+                  <div>
+                    <div
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      あなたのフィードバック
+                    </div>
+                    <textarea
+                      {...register("description", { required: true, maxLength: 500 })}
+                      id="description"
+                      name="description"
+                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    ></textarea>
+                    {errors.description &&
+                      "500文字以下で回答してください。"}
+                  </div>
+                </div>
+                <div className="p-2 w-full">
+                  <button
+                    type="submit"
+                    className="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded mt-8"
+                  >
+                    フィードバックを投稿する
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className="container px-5 mx-auto">
-        {props.answer.description}
-      </div>
-
-      <div>Feedbacks</div>
-      <div>
-        <ul>
-          {props.feedbacks.map((feedback) => (
-            <li key={feedback.id}>
-              {feedback.description}
-              <span></span>
-              <VoteButton
-                feedbackId={feedback.id}
-                userId={currentUser?.uid}
-                totalUpVotes={feedback.total_up_votes}
-                totalDownVotes={feedback.total_down_votes}
-                totalVotes={feedback.total_votes}
-                isVoted={votedFeedbackIds.includes(feedback.id)}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {/* フォーム */}
-      <div className="container px-5 mx-auto">
-        <div className="lg:w-1/2 md:w-2/3 mx-auto">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="p-2 w-full">
-              <div>
-                <div
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  あなたのフィードバック
-                </div>
-                <textarea
-                  {...register("description", { required: true, maxLength: 500 })}
-                  id="description"
-                  name="description"
-                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                ></textarea>
-                {errors.description &&
-                  "500文字以下で回答してください。"}
-              </div>
-            </div>
-            <div className="p-2 w-full">
-              <button
-                type="submit"
-                className="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-              >
-                フィードバックを投稿する
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      </Container>
     </>
   )
 }
